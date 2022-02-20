1c1e94a82ade3e86b90b25932c2b9621320244e9
Agustina Scarpinelli <agustina.scarpinelli@gmail.com>
2022-02-20 18:54:54 -0300

    Iniciando mi proyecto en js

---
+++ script.js
@@ -0,0 +1,34 @@
+let repetir=false;
+let usuarios=["agustina"];
+let inicio=false;
+let usuario;
+
+do{
+ usuario=parseInt(prompt ("Ingrese la opcion correcta:\n1) Ya estoy registrado \n2) Deseo registrarme"));
+if (usuario===1){
+    let usuarioViejo=prompt("Ingrese su numbre: ")
+    let indice=usuarios.indexOf(usuarioViejo)
+    if (indice!==-1){
+        alert(`Bienvenido nuevamente ${usuarioViejo}`)
+    }
+    else{
+        alert("Usuario no encontrado")
+        inicio=true;
+    }
+}
+else if(usuario===2){
+let nuevoUsuario=prompt("Ingrese su nombre: ")
+usuarios.push(nuevoUsuario)
+alert(`Bienvenido ${nuevoUsuario}`);
+inicio=false;
+}
+else{
+    alert("Opcion ingresada incorrecta")
+}
+}
+while (usuario<1 || usuario >2 || isNaN(usuario) || inicio===true);
+
+
+class Usuarios{
+    constructor(nombre, apellido,)
+}
