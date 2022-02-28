7ca61158183bf7f9d2be2cd93fefead81a5387ab
Agustina Scarpinelli <agustina.scarpinelli@gmail.com>
2022-02-27 21:43:21 -0300

    Agregando eventos

--- script_inicio.js
+++ script_inicio.js
@@ -1,3 +1,46 @@
-const mensajeBienvenida{
-    alert("Bienvenido nuevamente")
+
+localStorage.setItem('UsuariosRegistro',JSON.stringify(usuarios))
+ingresoFormulario.addEventListener('click',()=>{
+
+    let ingresoEmail=document.getElementById("usuarioEmail").value
+    let ingresoContraseña=document.getElementById("usuarioContraseña").value
+    let usuariosDeStorage=JSON.parse(localStorage.getItem('UsuariosRegistro'))
+    
+    do{
+           usuariosDeStorage.forEach(persona=>{
+           if (persona.email===ingresoEmail && persona.contraseña===ingresoContraseña){
+            console.log("usuario encontrado")
+            console.log(`Datos del usuario:/nNombre:${persona.nombre}:/nApellido:${persona.apellido}/nFecha de nacimiento:${persona.fechadenacimiento}`)
+            }
+    
+            else{
+               let alerta2=document.getElementById("alerta2")
+               alerta2.innerText="Usuario Incorrecto"
+                ingreso=false;
+                ingresoFormulario.reset()
+            }
+     })
+    } while(ingreso===false)
+
+
+})
+let enviarEmail=document.getElementById("enviarEmail")
+let divEmail=document.getElementById("divEmail")
+
+enviarEmail.addEventListener('click',()=>{
+divEmail.innerHTML=`
+<label for="email" name="enviarEmail" id="enviarEmail">Ingresa tu email</label>
+<input type="text"name="emailVerficacion" id="emailVerificacion" placeholder="Email">
+<button type="submit" name="botonVerificacion" id ="botonVerificacion">Enviar</button>
+`
+})
+
+let botonVerificacion=document.getElementById("botonVerificacion")
+
+botonVerificacion.addEventListener('click', ()=>{
+
+ divEmail.innerHTML=`
+ <p>Se ha enviado un email para recuperar la contraseña</p>`
+
 }
+)
