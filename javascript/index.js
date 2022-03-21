31eaa7ca2afd22faeddd3ed27bf853f43e02b20f
Agustina Scarpinelli <agustina.scarpinelli@gmail.com>
2022-03-20 23:12:16 -0300

    Incorporando products.json

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,13 @@
 Swal.fire({
     html:
-    '<p>Ingresando a tu cuenta en MUB tenes un 10%OFF en el total de tu compra</p>'+
+    '<p color:"#C24660">Ingresando a tu cuenta en MUB tenes un 10%OFF en el total de tu compra</p>'+
     '</br>'+
-    '<a href="signup.html">Suscribirme</a> ' +
-    '<a href="signin.html">Ingresar</a> ',
+    '<a class="button2" href="signup.html">Suscribirme</a> ' +
+    '<a class="button2" href="signin.html">Ingresar</a> ',
     imageUrl: 'imagenes/logodiscount.png',
-    imageWidth: 200,
-    imageHeight: 200,
-    imageAlt: 'Custom image',
+    imageWidth: 150,
+    imageHeight: 150,
+    imageAlt: 'logo discount',
+    background:"#D6B4B2",
+    
   })
--- javascript/makeup.js
+++ javascript/makeup.js
@@ -10,15 +10,7 @@ class Makeup{
     }
 }
 
-const makeup= [];
 
-let lorealLipStick=new Makeup("Loreal labial liquido", 01,"Labial liquido larga duracion Loreal Paris x 7 gr","imagenes/img-01/loreal_lip01.jpg", 998.50,10,0101)
-let rimmelLipStick=new Makeup("Rimmel lapiz labial", 01,"Lapiz labial Rimmel London x 4,2 gr","imagenes/img-01/rimmel-lipstick-01.jpg",1350,10,0102)
-let macLipStick=new Makeup("Mac lapiz labial",01,"Lapiz labial Mac Ultra duracion x 5 gr","imagenes/img-01/mac_lip01.jpg",1890,10,0103)
-let maybellineLipStick=new Makeup("Mayabelline labial liquido",01,"Labial liquido Maybelline Superstay x 8 gr","imagenes/img-01/maybelline_lip01.jpg",1500,10,0104)
-let covergirlLipStick=new Makeup("Covergirl labial liquido",01,"Labial liquido Covergirl matte x 7,5 gr ","imagenes/img-01/covergirl_lip01.jpg",1750,10,0105)
-
-makeup.push(lorealLipStick,rimmelLipStick,macLipStick,maybellineLipStick,covergirlLipStick)
 
 const container=document.querySelector(".container")
 const sidebar=document.querySelector(".sidebar")
@@ -31,20 +23,27 @@ let shoppingCart=JSON.parse(localStorage.getItem("shoppingCart")) || [];
 
  })
 let divProducts=document.getElementById('makeup-products');
-const showProducts=()=>{
-        makeup.forEach(product => {
-         
-        divProducts.innerHTML+=`
-        <div class="card card${product.id}" style="width: 18rem;">
-  <div class="img-prod-div"><img src=${product.img} class="imgProd card-img-top" alt="..."> <button class="color "></button><button class="color "></button><button class="color"></button></div>
-  <div class="card-body">
-    <h5 class="title">${product.title}</h5>
-    <p class="description">${product.description}</p>
-    <p class="price">$ ${product.price}</p>
-    <button class="btnAdd btn btn-primary" data-id="${product.id}">Agregar al carrito</button>
-  </div>
-</div>`        
-    });
+
+async function showProducts(){
+       const response=await fetch('./json/products.json')
+       return await response.json();
+}
+showProducts().then(products=>{
+  products.forEach((product,index)=>{
+    divProducts.innerHTML+=`
+    <div class="card card${product.id}" style="width: 18rem;">
+<div class="img-prod-div"><img src="./imagenes/img-01/${product.img}" class="imgProd card-img-top" alt="..."> <button class="color "></button><button class="color "></button><button class="color"></button></div>
+<div class="card-body">
+<h5 class="title">${product.title}</h5>
+<p class="description">${product.description}</p>
+<p class="price">$ ${product.price}</p>
+<button class="btnAdd btn btn-primary" data-id="${product.id}">Agregar al carrito</button>
+</div>
+</div>` 
+  })
+})
+
+
     const btnAdd=document.querySelectorAll(".btnAdd")
     btnAdd.forEach((e)=>
         e.addEventListener("click",(e)=>{
@@ -52,7 +51,7 @@ const showProducts=()=>{
             addToShoppingCart(card);
         })
     )
-}
+
   showProducts();
 
   const addToShoppingCart=(card)=>{
